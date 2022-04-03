7fbd2e1d428171ccd60ddfe2422e4459a646801b
Furkan Güler <furkang2k20@outlook.com>
2022-04-03 21:03:59 +0300

    tiny footer changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,23 @@
 import authHelpers from "../authHelpers";
 
 const footer = (props) => {
-  const {logged} = props;
+  const { logged } = props;
   return (
     <footer>
       {logged ? <div className="logout"><button onClick={e => authHelpers.logout()}>&gt; log out</button></div> : <div className="logout"></div>}
       <div className="github"><button onClick={footer.goToRepo}></button></div>
       <div className="info">
         <div className="spoti">Spotify API is a shy one, so bear with me.</div>
-        <div className="react">made with <h3>React</h3>.. and some coffee.</div>
+        <div className="react">made with <h3 onClick={footer.goToReact}>React</h3>.. and some <h2>☕</h2></div>
       </div>
 
     </footer>
   )
+}
+
+footer.goToReact = () => {
+  let url = 'https://reactjs.org'
+  window.open(url, '_blank');
 }
 
 footer.goToRepo = () => {
